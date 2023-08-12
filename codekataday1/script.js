3d112bc0d2bc3f528484a09fc60f95b0c8271bfc
vidhyavkm@gmail.com <vidhyavkm@gmail.com>
2023-08-12 12:25:14 +0530

    firstday

---
+++ codekataday1/script.js
@@ -0,0 +1,22 @@
+// var array = [1,'2', "guvi",true]
+
+var str= 10;
+console.log(str);
+
+
+var arr = "hello"
+console.log(arr);
+
+
+// var str1= guvi;
+// console.log(str1);
+
+//for loop
+
+//exp 1
+
+var arr = userInput[0].split("");
+for (var i=0; i<arr.length; i++ ){
+    console.log(arr[i]);
+}
+
