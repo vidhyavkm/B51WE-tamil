1699881e2bc7827a52c30ddbacf5b8b91820942a
vidhyavkm@gmail.com <vidhyavkm@gmail.com>
2023-08-13 11:34:57 +0530

    date13

--- codekataday1/script.js
+++ codekataday1/script.js
@@ -1,11 +1,11 @@
 // var array = [1,'2', "guvi",true]
 
-var str= 10;
-console.log(str);
+// var str= 10;
+// console.log(str);
 
 
-var arr = "hello"
-console.log(arr);
+// var arr = "hello"
+// console.log(arr);
 
 
 // var str1= guvi;
@@ -15,8 +15,56 @@ console.log(arr);
 
 //exp 1
 
-var arr = userInput[0].split("");
-for (var i=0; i<arr.length; i++ ){
-    console.log(arr[i]);
-}
+// var arr = userInput[0].split("");
+// for (var i=0; i<arr.length; i++ ){
+//     console.log(arr[i]);
+// }
 
+//join method
+// var arr = "fire";
+
+// console.log(arr.join())
+
+// console.log(arr.join(''))
+
+// console.log(arr.join('-'))
+
+// console.log(arr.join(' '))
+
+//split method
+
+// let str = "hello guvi students B51WE Tamil";
+
+// let words = str.split();
+// console.log(words[0]);
+
+// let words1 = str.split('');
+// console.log(words1[0]);
+
+// let words2 = str.split(' ');
+// console.log(words2[0]);
+
+//forloop
+
+// for (let i=0; i<1000; i++){
+//     console.log(i);
+// }
+
+// for (let i=100; i>50; i--){
+//     console.log(i);
+// }
+
+let string = "";
+
+for (let i=0; i<10; i++){
+   string = string + i;
+}
+console.log(string);
+
+
+
+let string1 = "helloworld";
+
+for (let i=0; i<10; i++){
+    console.log(string1[i]);
+}
