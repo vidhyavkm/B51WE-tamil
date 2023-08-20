0b3a051f0bd56d32d3ddab040df4b1a85d80ed48
vidhyavkm@gmail.com <vidhyavkm@gmail.com>
2023-08-20 10:16:11 +0530

    new

---
+++ codekata/script.js
@@ -0,0 +1,21 @@
+var num = 2;
+console.log(num);
+
+// console.log("1");
+
+// console.log("2");
+
+// console.log("3");
+
+// console.log("4");
+
+// console.log("5");
+
+for (var i=1; i<=50; i++){
+    console.log(i)
+}
+
+var string = "guvigeek";
+
+
+
