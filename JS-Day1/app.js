40b298b2c4b88fee32751e117653e97957c8b47e
vidhyavkm <85151120+vidhyavkm@users.noreply.github.com>
2023-08-27 12:45:21 +0530

    Add files via upload

---
+++ JS-Day1/app.js
@@ -0,0 +1,16 @@
+var str ="guvi";
+console.log(str);
+
+var studetails = {
+    fname:"vidhya",
+    lname:"selvam",
+    age:30,
+    mail:"vidhya@gmail.com",
+    hobbies:['singing','programming']
+}
+
+console.log(studetails)
+
+console.log(studetails.mail)
+
+console.log(studetails["age"])
